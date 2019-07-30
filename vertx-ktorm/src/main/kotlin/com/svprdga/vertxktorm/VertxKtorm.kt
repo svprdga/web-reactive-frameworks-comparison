366e597b5119af6db33e19869b6bd25dd99190b7
svprdga <svprdga@gmail.com>
2019-07-30 20:10:28 +0200

    Trying to deploy the Vert.x's...

--- vertx-ktorm/src/main/kotlin/com/svprdga/vertxktorm/VertxKtorm.kt
+++ vertx-ktorm/src/main/kotlin/com/svprdga/vertxktorm/VertxKtorm.kt
@@ -4,8 +4,13 @@ import com.svprdga.vertxktorm.verticle.GalaxyVerticle
 import com.svprdga.vertxktorm.verticle.MainVerticle
 import io.vertx.core.Vertx
 
-fun main(args: Array<String>) {
-    val vertx = Vertx.vertx()
-    vertx.deployVerticle(GalaxyVerticle())
-    vertx.deployVerticle(MainVerticle())
+open class Application {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val vertx = Vertx.vertx()
+            vertx.deployVerticle(GalaxyVerticle())
+            vertx.deployVerticle(MainVerticle())
+        }
+    }
 }
